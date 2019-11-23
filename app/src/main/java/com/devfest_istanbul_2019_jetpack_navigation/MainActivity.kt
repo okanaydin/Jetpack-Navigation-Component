0407887fb495229860c471bc8955635837d42c8c
okanaydin <okan.aydin@isik.edu.tr>
2019-11-23 14:20:36 +0300

    Initial commit

---
+++ app/src/main/java/com/devfest_istanbul_2019_jetpack_navigation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.devfest_istanbul_2019_jetpack_navigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
